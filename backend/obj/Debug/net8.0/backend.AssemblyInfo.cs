650bda21ead98b21773c6390b5b2fe18eceedb70
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2024-12-04 21:55:36 +0800

    Updated deploy.yml file

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9138cb7b3f259ca270e6407fbf42ed25b8e7e5d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a76d80ec3be82c031e46cd5cb5e8284a72efddfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
